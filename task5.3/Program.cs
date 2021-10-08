972ff16f7ec0a5a08da1528a1b98dbdebb950b7c
Ангеліна Феценець <Anhelina.Fetsenets.PZ.2020@edu.lpnu.ua>
2021-10-08 20:48:28 +0300

    Add project files.

---
+++ task5.3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace task5._3
+{
+    class Path
+    {
+        private string filepath;
+        private string rootfolder;
+        private string filename;
+
+        public Path(string path)
+        {
+            this.filepath = path;
+            string[] subs = path.Split("\\");
+            this.rootfolder = subs[0];
+            string[] sub2 = subs[subs.Length - 1].Split(".");
+            this.filename = sub2[0];
+        }
+
+        public override string ToString()
+        {
+            return $@"Full path: {filepath} Root folder: {rootfolder} Files name: {filename}";
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Path a = new Path(Console.ReadLine());
+            Console.WriteLine(a);
+        }
+    }
+}
